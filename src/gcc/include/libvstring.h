4f2bb725bf71b26fd7108d458f3b6c6fe9201258
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2017-10-15 10:20:36 +0200

    Added StrSize(), StrSizeW(), StrSizeL().

--- src/gcc/include/libvstring.h
+++ src/gcc/include/libvstring.h
@@ -2,11 +2,27 @@
 
 #include <exec/types.h>
 
+/* memory blocks */
+
 APTR AllocVecPooled(__reg("a0") APTR pool, __reg("d0") ULONG size);
 APTR CloneMem(__reg("a0") const APTR src, __reg("d0") ULONG size);
 APTR CloneMemPooled(__reg("a0") APTR pool, __reg("a1") const APTR src, __reg("d0") ULONG size);
 void FreeVecPooled(__reg("a0") APTR pool, __reg("a1") APTR block);
+
+/* string copy */
+
 UBYTE* StrCopy(__reg("a0") const UBYTE *s, __reg("a1") UBYTE *d);
 UWORD* StrCopyW(__reg("a0") const UWORD *s, __reg("a1") UWORD *d);
 ULONG* StrCopyL(__reg("a0") const ULONG *s, __reg("a1") ULONG *d);
+#define StrCopyU(s) StrCopy(s)
+
+/* string size */
+
+ULONG StrSize(__reg("a0") const UBYTE *s);
+ULONG StrSizeW(__reg("a0") const UWORD *s);
+ULONG StrSizeL(__reg("a0") const ULONG *s);
+#define StrSizeU(s) StrSize(s)
+
+/* UTF-8 support */
+
 ULONG Utf8Size(__reg("d0") ULONG cp);
