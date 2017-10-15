bb43971e5943fd646db04206f61245f048d27695
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2017-10-15 22:41:34 +0200

    Added StrCount(), StrCountW(), StrCountL().

--- src/gcc/include/libvstring.h
+++ src/gcc/include/libvstring.h
@@ -27,13 +27,23 @@ UWORD* StrClonePooledW(__reg("a0") APTR pool, __reg("a1") const UWORD *s);
 ULONG* StrClonePooledL(__reg("a0") APTR pool, __reg("a1") const ULONG *s);
 #define StrClonePooledU(p,s) StrClonePooled(p,s)
 
-/* string size */
+/* string size and character count */
 
 ULONG StrSize(__reg("a0") const UBYTE *s);
 ULONG StrSizeW(__reg("a0") const UWORD *s);
 ULONG StrSizeL(__reg("a0") const ULONG *s);
 #define StrSizeU(s) StrSize(s)
+LONG StrCount(__reg("a0") const UBYTE *s);
+/* LONG StrCountU(__reg("a0") const UBYTE *s); */
+LONG StrCountW(__reg("a0") const UWORD *s);
+LONG StrCountL(__reg("a0") const ULONG *s);
 
 /* UTF-8 support */
 
 ULONG Utf8Size(__reg("d0") ULONG cp);
+
+/* error codes */
+
+#define EVSTR_U16_LOW_SURROGATE_UNEXPECTED    -1
+#define EVSTR_U16_LOW_SURROGATE_MISSING       -2
+#define EVSTR_U32_OUT_OF_RANGE                -3
